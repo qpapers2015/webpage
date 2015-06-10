ed432825eeca5339bcdf63521375c8c3a33c108f
EDGE1095 <abhkamath@gmail.com>
2015-06-11 01:17:27 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("GATE").onclick=function(){
+	document.getElementById("content").style.display="none";
+	document.getElementById("quote").style.display="none";
+	document.getElementById("body_frame").style.display="block";
+}
