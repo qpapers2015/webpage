a87f4f66219e35ab1498e0c567fb71288daf7c6b
EDGE1095 <abhkamath@gmail.com>
2015-06-11 11:29:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ document.getElementById("GATE").onclick=function(){
 	document.getElementById("quote").style.display="none";
 	document.getElementById("body_frame").style.display="block";
 }
+
+document.getElementById("HOME").onclick=function(){
+	document.getElementById("content").style.display="block";
+	document.getElementById("quote").style.display="block";
+	document.getElementById("body_frame").style.display="none";
+	}
