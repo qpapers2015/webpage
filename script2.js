b0bee2c42ca673c1029a9843b70e28935387a2ba
Sangita Samota <samota1295@users.noreply.github.com>
2015-06-12 20:16:56 +0530

    Create script2.js

---
+++ script2.js
@@ -0,0 +1,16 @@
+document.getElementById("AB").onclick=function(){
+	document.getElementById("AB2").style.display="block";
+}
+document.getElementById("Samota").onclick=function(){
+	document.getElementById("Samota2").style.display="block";
+}
+	
+document.getElementById("Ken").onclick=function(){
+	document.getElementById("Ken2").style.display="block";
+}
+	
+document.getElementById("Dnvasa").onclick=function(){
+	document.getElementById("Dnvasa2").style.display="block";
+	
+}
+	
