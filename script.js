8e55e8e46050735c4a3108125947f4ec6171ea82
EDGE1095 <abhkamath@gmail.com>
2015-06-12 10:50:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,15 +2,20 @@ document.getElementById("GATE").onclick=function(){
 	document.getElementById("content").style.display="none";
 	document.getElementById("quote").style.display="none";
 	document.getElementById("body_frame").style.display="block";
+	
+	document.getElementById("body_frame").setAttribute("src","GATE.html");
+	
 }
 
 document.getElementById("HOME").onclick=function(){
 	document.getElementById("content").style.display="block";
 	document.getElementById("quote").style.display="block";
 	document.getElementById("body_frame").style.display="none";
+}
 	
 document.getElementById("AboutUs").onclick=function(){
 	document.getElementById("content").style.display="none";
 	document.getElementById("quote").style.display="none";
 	document.getElementById("body_frame").style.display="block";	
-	}
+	document.getElementById("body_frame").setAttribute("src","AboutUs.html");
+}
