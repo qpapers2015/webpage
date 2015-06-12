ea1f8d844a8790e4a7713f9a750f3a859e6e6311
EDGE1095 <abhkamath@gmail.com>
2015-06-13 01:13:45 +0530

    Create Sections_script.js

---
+++ Sections_script.js
@@ -0,0 +1,7 @@
+
+document.getElementById("Engineering").onclick=function(){
+	document.getElementById("Engineering_word").style.display="none";
+	document.getElementById("hidden").style.display="block";
+	
+	
+}
